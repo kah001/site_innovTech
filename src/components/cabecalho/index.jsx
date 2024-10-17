34ce675f6129e53188174b9e3be55be1f1fa3ea5
kahvicente <karinasilvavicente007@gmail.com>
2024-10-17 00:40:26 -0300

    menu responsivo finalizado

--- src/components/cabecalho/index.jsx
+++ src/components/cabecalho/index.jsx
@@ -1,15 +1,28 @@
 import './index.scss'
+import { useState } from 'react'
 
 export default function Cabecalho() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    function toggleMenu() {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <div className='comp-cabecalho'>
             <img src="/assets/images/innovTech.png" alt="" />
 
-            <div className='topicos'>
+            <nav className={`nav ${isOpen ? 'open' : ''}`}>
                 <a href='#inicio'>INÍCIO</a>
                 <a href='#sobre'>SOBRE</a>
                 <a href='#vantagens'>VANTAGENS</a>
                 <a href="#equipe">EQUIPE</a>
+            </nav>
+
+            <div className='menu-icon' onClick={toggleMenu}>
+                <div className={`menu ${isOpen ? 'toggle' : ''}`}></div>
+                <div className={`menu ${isOpen ? 'toggle' : ''}`}></div>
+                <div className={`menu ${isOpen ? 'toggle' : ''}`}></div>
             </div>
         </div>
     )
