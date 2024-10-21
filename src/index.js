c00d7ee00a182feb2eb2ab766406701745a2c854
kah001 <karinasilvavicente15@gmail.com>
2024-10-21 16:34:31 -0300

    iniciando pagina sobre nos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import Navegacao from './routes';
 
+//importacoes do swiper para o carousel
+import 'swiper/css'
+import 'swiper/css/navigation' //modulos das setas
+import 'swiper/css/pagination' //importando as bolinhas
+import 'swiper/css/scrollbar'
+import 'swiper/swiper-bundle.css'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/sobreNos/index.jsx
+++ src/pages/sobreNos/index.jsx
@@ -1,9 +1,16 @@
 import './index.scss'
+import Cabecalho from '../../components/cabecalho'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Pagination, Navigation } from 'swiper/modules'
 
 export default function SobreNos() {
     return(
         <div className='pagina-sobre-nos'>
-            
+            <Cabecalho />
+
+            <section className='sobre'></section>
+
+            <section className='equipe'></section>
         </div>
     )
 }
